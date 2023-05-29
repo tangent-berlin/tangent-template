8000c1eb7a53ec246b73afd157c1edd2ddf3a246
Alex Steinke <alex@tangent.center>
2023-05-29 16:28:51 +0200

    feat: devcontainer

--- .devcontainer/config/config.fish
+++ .devcontainer/config/config.fish
@@ -1,15 +1,6 @@
 # Disable greeting
 set -g fish_greeting
 
-set -gx HOME "/root"
-set -gx XDG_CONFIG_HOME "/root/.config"
-set -gx XDG_CACHE_HOME "/root/.cache"
-set -gx INSTALLDIR "/opt"
-
-set --export PATH "$INSTALLDIR/cargo/bin" $PATH
-set --export PATH "$INSTALLDIR/poetry/bin" $PATH
-set --export PATH "$INSTALLDIR/bun/bin" $PATH
-
 function ls
     lsd
     #python3 /usr/local/bin/lsicons.py
